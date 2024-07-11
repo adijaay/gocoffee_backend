daeb5f92e1aed3b4648fde522626b50b163e3a8e
adijaay <adiwijayaswastama@gmail.com>
2024-07-11 21:28:18 +0700

    change path firebase sdk

--- src/firebase/admin-firebase.js
+++ src/firebase/admin-firebase.js
@@ -1,8 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-sdk-server.json");
-
-// serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY;
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
