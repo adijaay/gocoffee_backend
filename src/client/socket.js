9223dea3693c90631fa3e4ada29fe2bd17ad4a91
adijaay <adiwijayaswastama@gmail.com>
2024-07-11 15:48:13 +0700

    selected keys

--- src/client/socket.js
+++
@@ -1,8 +0,0 @@
-import io from "/node_modules/socket.io-client/dist/socket.io.js";
-
-const socket = io("http://localhost:3000"); // Replace with your server URL
-
-socket.on("61-order-completed", (order) => {
-  console.log("Order completed:", order);
-  // Handle the order data as needed
-});
--- src/firebase/admin-firebase.js
+++ src/firebase/admin-firebase.js
@@ -2,6 +2,8 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require("./go-coffee-firebase-adminsdk.json");
 
+serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY;
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
