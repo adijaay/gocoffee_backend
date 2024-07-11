758386281603031c74b4b4b0d6189d27759fc15f
adijaay <adiwijayaswastama@gmail.com>
2024-07-11 21:16:14 +0700

    change sdk

--- src/firebase/admin-firebase.js
+++ src/firebase/admin-firebase.js
@@ -1,8 +1,8 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./go-coffee-firebase-adminsdk.json");
+var serviceAccount = require("./firebase-sdk-server.json");
 
-serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY;
+// serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
